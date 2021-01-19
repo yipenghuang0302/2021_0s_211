8e94ba7ece4653cf409ee7d656eee5b47b8af6db
Yipeng Huang <yipeng.huang@rutgers.edu>
2021-01-19 00:50:23 -0500

    Creating assignment_autograder.py

---
+++ pa1/assignment_autograder.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from goldbach import autograder as goldbach_autograder
+from maximum import autograder as maximum_autograder
+from balanced import autograder as balanced_autograder
+from bstReverseOrder import autograder as bstReverseOrder_autograder
+
+total = 0
+
+total += goldbach_autograder.grade_goldbach ( prefix="goldbach/", verbose=False )
+total += maximum_autograder.grade_maximum ( prefix="maximum/", verbose=False )
+total += balanced_autograder.grade_balanced ( prefix="balanced/", verbose=False )
+total += bstReverseOrder_autograder.grade_bstReverseOrder ( prefix="bstReverseOrder/", verbose=False )
+
+print ("Score on assignment: {} out of 108.".format(total))
+print ("The remaining 12 points will be assigned as part of recitation discussion.")
