98a966a84ae4be8444f07d032f0d27801faaca27
Yipeng Huang <yipeng.huang@rutgers.edu>
2021-02-11 20:01:08 -0500

    Adding edgelist.

--- pa2/edgelist/edgelist_provided.c
+++ pa2/edgelist/edgelist_provided.c
@@ -5,27 +5,14 @@ int main ( int argc, char* argv[] ) {
 
     // FIRST, READ THE ADJACENCY MATRIX FILE
     AdjacencyListNode* adjacencyList = NULL;
-    size_t graphNodeCount = adjMatrixToList (
-        argv[1],
-        &adjacencyList
-    );
+    /* ... */
 
     // NEXT, TRAVERSE THE ADJACENCY LIST AND PRINT EACH EDGE, REPRESENTED AS A PAIR OF NODES
     // Example of traversing the adjacency list is in the freeAdjList() function in graphutils.h
-    for (size_t source=0; source<graphNodeCount; source++) {
-        AdjacencyListNode* dest = adjacencyList[source].next;
-        while (dest) {
-            printf("%ld %ld\n",source,dest->graphNode);
-            // iterator
-            dest = dest->next;
-        }
-    }
+    /* ... */
 
     // NOW, BE SURE TO FREE THE ADJACENCY LIST
-    freeAdjList (
-        graphNodeCount,
-        adjacencyList
-    );
+    /* ... */
 
     return EXIT_SUCCESS;
 }
