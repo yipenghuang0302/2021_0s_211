24079c855bcd6cdc8b2d80e599d518c79c701c48
Yipeng Huang <yipeng.huang@rutgers.edu>
2021-04-07 23:41:22 -0400

    Adding fullyAssociative.

---
+++ pa5/fullyAssociative/autograder.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+import re
+import os
+import datetime
+import subprocess
+
+def generate_matMul_trace ( filenum, dimension=2, path="./" ):
+
+    with open("{}tests/test{}.txt".format(path,filenum), "w") as infile:
+        trace = subprocess.run(
+            ['valgrind', '--tool=lackey', '--basic-counts=no', '--trace-mem=yes', '--log-fd=1', '../matMul/matMul', str(dimension)],
+            cwd=path,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding='ASCII',
+            timeout=datetime.timedelta(seconds=4).total_seconds(),
+        )
+
+        with open(".marker".format(path,filenum), "r") as marker_file:
+            start = int(marker_file.readline(),16)
+            end = int(marker_file.readline(),16)
+
+        is_relevant_region = False
+        for line in trace.stdout.splitlines():
+            if line[1]=='L' or line[1]=='S' or line[1]=='M':
+                addr = int(re.split(' |,',line)[2],16)
+                if addr==start:
+                    is_relevant_region = True
+                elif addr==end:
+                    is_relevant_region = False
+                elif is_relevant_region and addr < 0xffff_ffff:
+                    infile.write(line+'\n')
+                else:
+                    # print(line)
+                    pass
+            elif not line[0]=='I':
+                # print(line)
+                pass
+
+def answers_from_csim ( filenum, path="./" ):
+
+    with open("{}answers/answer{}.txt".format(path,filenum), "w") as outfile:
+        csim = subprocess.run(
+            ['../csim-ref', '-s', '0', '-E', '16', '-b', '8', '-l', '0', '-t', "{}tests/test{}.txt".format(path,filenum)],
+            cwd=path,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding='ASCII',
+            timeout=datetime.timedelta(seconds=4).total_seconds(),
+        )
+        outfile.write(csim.stdout)
+
+def generate_test_suite():
+
+    subprocess.run( ['make', '-B', '-C', '../matMul'], cwd='./', check=True, )
+    os.makedirs("tests", exist_ok=True)
+    os.makedirs("answers", exist_ok=True)
+
+    generate_matMul_trace ( 4, 1 )
+    generate_matMul_trace ( 5, 2 )
+    generate_matMul_trace ( 6, 16 )
+    generate_matMul_trace ( 7, 32 )
+    for filenum in range(8):
+        answers_from_csim ( filenum )
+
+def test_fullyAssociative ( filenum, path="./", verbose=False ):
+
+    try:
+        with open("{}answers/answer{}.txt".format(path,filenum), "r") as outfile:
+            answer = outfile.read()
+    except EnvironmentError: # parent of IOError, OSError
+        print ("answers/answer{}.txt missing".format(filenum))
+
+    try:
+        result = subprocess.run(
+            ['./fullyAssociative', "tests/test{}.txt".format(filenum)],
+            cwd=path,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding='ASCII',
+            timeout=datetime.timedelta(seconds=4).total_seconds(),
+        )
+        if verbose:
+            print (' '.join(result.args))
+            print ("answer")
+            print (answer)
+            print ("result")
+            print (result.stdout)
+        assert answer == result.stdout, "The printed result doesn't match answers/answer{}.txt.".format(filenum)
+        return True
+    except subprocess.CalledProcessError as e:
+        # print (e.output)
+        print ("Calling ./fullyAssociative returned non-zero exit status.")
+    except ValueError as e:
+        print (result.stdout)
+        print ("Please check your output formatting; it should be formatted as a hexadecimal number.")
+    except AssertionError as e:
+        print (result.stdout)
+        print (e.args[0])
+
+    return False
+
+def grade_fullyAssociative( path='./', verbose=False ):
+
+    score = 0
+
+    try:
+        subprocess.run( ['make', '-B'], cwd=path, check=True, )
+    except subprocess.CalledProcessError as e:
+        print ("Couldn't compile fullyAssociative.")
+        return score
+
+    for filenum in range(8):
+        if test_fullyAssociative(filenum,path,verbose):
+            score += 3
+        else:
+            break
+
+    print ("Score on fullyAssociative: {} out of 24.".format(score))
+    return score
+
+if __name__ == '__main__':
+    # generate_test_suite()
+    grade_fullyAssociative(verbose=True)
+    exit()
---
+++ pa5/fullyAssociative/fullyAssociative_provided.c
@@ -0,0 +1,124 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Rutgers University 2021 Spring CS 211 Computer Architecture
+// Assignment based on CS:APP Cache Lab
+
+// 64-bit memory address
+typedef unsigned long long int mem_addr_t;
+
+// cache properties
+unsigned char s = 0; // set index bits
+unsigned short S = 1<<0; // set count
+
+unsigned char E = 16; // way associative cache; lines per set
+
+unsigned char b = 8; // block offset bits
+// unsigned short B = 1<<8; // block size in bytes
+
+// fully associative cache line
+typedef struct cache_line {
+    mem_addr_t tag;
+    struct cache_line* next_cache_line;
+} cache_line_t;
+
+typedef struct fifo_cache_set {
+    cache_line_t* front; // front (head) of the queue
+    cache_line_t* back; // back (tail) of the queue
+    unsigned char occupancy;
+} fully_associative_fifo_cache_t;
+
+// accessData - Access data at memory address addr.
+void accessData (
+    mem_addr_t addr,
+    fully_associative_fifo_cache_t* cache,
+    unsigned int* hit_count, // If it is already in cache, increase hit_count
+    unsigned int* miss_count, // If it is not in cache, bring it in cache, increase miss_count
+    unsigned int* eviction_count // Also increase eviction_count if a line is evicted
+) {
+
+    // Cache indices for this address
+    mem_addr_t tag = addr >> (s+b);
+
+    // Cache hit
+    cache_line_t* curr_line = cache->front;
+    while ( curr_line != NULL ) {
+        if ( curr_line->tag == tag ) {
+            (*hit_count)++;
+            return;
+        }
+        curr_line = curr_line->next_cache_line;
+    }
+
+    // Otherwise, record a cache miss
+    /* ... */
+
+    // If cache is full, evict oldest line due to FIFO cache replacement policy
+    if ( cache->occupancy == E ) {
+        // dequeue from front of FIFO, update occupancy, and record an eviction
+        /* ... */
+    }
+
+    // Due to cache miss, enqueue cache line, and update occupancy
+    /* ... */
+
+}
+
+int main(int argc, char* argv[]) {
+
+    // path to memory trace
+    if ( argc!= 2 ) {
+        printf( "Usage: ./fullyAssociative <mem_trace_file>\n" );
+        exit( EXIT_FAILURE );
+    }
+    char* mem_trace_file = argv[1];
+    FILE *fp = fopen(mem_trace_file, "r");
+    if (!fp) {
+        fprintf(stderr, "Error opening file '%s'\n", mem_trace_file);
+        exit( EXIT_FAILURE );
+    }
+
+    fully_associative_fifo_cache_t cache = { .front=NULL, .back=NULL, .occupancy=0 };
+    // cache simulation statistics
+    unsigned int miss_count = 0;
+    unsigned int hit_count = 0;
+    unsigned int eviction_count = 0;
+
+    // Loop through until we are done with the file.
+    size_t line_buf_size = 256;
+    char line_buf[line_buf_size];
+
+    while ( fgets(line_buf, line_buf_size, fp) != NULL ) {
+
+        // replay the given trace file against the cache
+        if ( line_buf[1]=='L' || line_buf[1]=='S' || line_buf[1]=='M' ) {
+            char access_type = '\0';
+            mem_addr_t addr = 0;
+            unsigned int len = 0;
+            sscanf ( line_buf, " %c %llx,%u", &access_type, &addr, &len );
+
+            if ( access_type=='L' || access_type=='S' || access_type=='M') {
+                accessData(addr, &cache, &hit_count, &miss_count, &eviction_count);
+            }
+
+            // If the instruction is M indicating L followed by S then access again
+            if(access_type=='M')
+                accessData(addr, &cache, &hit_count, &miss_count, &eviction_count);
+        }
+    }
+
+    cache_line_t* curr_line = cache.front;
+    while ( curr_line != NULL ) {
+        cache_line_t* temp = curr_line;
+        curr_line = curr_line->next_cache_line;
+        free(temp);
+    }
+    fclose(fp);
+
+    /* Output the hit and miss statistics for the autograder */
+    printf("hits:%d misses:%d evictions:%d\n", hit_count, miss_count, eviction_count);
+
+    exit( EXIT_SUCCESS );
+}
