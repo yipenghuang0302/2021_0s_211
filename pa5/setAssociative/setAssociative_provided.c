ebe6a01632d40eec0848c5d5e570d3b75b9fd0fc
Yipeng Huang <yipeng.huang@rutgers.edu>
2021-04-08 01:17:01 -0400

    Adding setAssociative.

---
+++ pa5/setAssociative/setAssociative_provided.c
@@ -0,0 +1,111 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Rutgers University 2021 Spring CS 211 Computer Architecture
+// Assignment based on CS:APP Cache Lab
+
+// 64-bit memory address
+typedef unsigned long long int mem_addr_t;
+
+// cache properties
+unsigned char s = 4; // set index bits
+unsigned short S = 1<<4; // set count
+
+unsigned char E = 16; // way associative cache; lines per set
+
+unsigned char b = 8; // block offset bits
+// unsigned short B = 1<<8; // block size in bytes
+
+// fully associative cache line
+typedef struct cache_line {
+    bool valid;
+    mem_addr_t tag;
+    size_t recent_use; // For keeping track of recent use of cache line
+} cache_line_t;
+
+typedef cache_line_t* cache_set_t;
+typedef cache_set_t* cache_t;
+
+// For keeping track of recent use of cache line
+size_t program_counter = 0;
+
+// accessData - Access data at memory address addr.
+void accessData (
+    mem_addr_t addr,
+    cache_t cache,
+    unsigned int* hit_count, // If it is already in cache, increase hit_count
+    unsigned int* miss_count, // If it is not in cache, bring it in cache, increase miss_count
+    unsigned int* eviction_count // Also increase eviction_count if a line is evicted
+) {
+
+    // Cache indices for this address
+    /* ... */
+
+    // Find the correct cache set
+    /* ... */
+
+    // Cache hit
+    /* ... */
+
+    // Otherwise, record a cache miss
+    /* ... */
+
+    // Within the correct cache set, find the least recently used line; that is where missed cache block will be placed
+    /* ... */
+
+    // If cache set line already in use, then record an eviction
+    /* ... */
+
+    /* ... */
+}
+
+int main(int argc, char* argv[]) {
+
+    // path to memory trace
+    if ( argc!= 2 ) {
+        printf( "Usage: ./fullyAssociative <mem_trace_file>\n" );
+        exit( EXIT_FAILURE );
+    }
+    char* mem_trace_file = argv[1];
+    FILE *fp = fopen(mem_trace_file, "r");
+    if (!fp) {
+        fprintf(stderr, "Error opening file '%s'\n", mem_trace_file);
+        exit( EXIT_FAILURE );
+    }
+
+    // Allocate memory, write 0's for valid and tag and LRU
+    cache_t cache = calloc( S, sizeof(cache_set_t) );
+    for (size_t set_index=0; set_index<S; set_index++) {
+        cache[set_index] = calloc( E, sizeof(cache_line_t) );
+    }
+
+    // cache simulation statistics
+    unsigned int miss_count = 0;
+    unsigned int hit_count = 0;
+    unsigned int eviction_count = 0;
+
+    // Loop through until we are done with the file.
+    size_t line_buf_size = 256;
+    char line_buf[line_buf_size];
+    while ( fgets(line_buf, line_buf_size, fp) != NULL ) {
+
+        program_counter++;
+
+        /* ... */
+    }
+
+    for (size_t set_index=0; set_index<S; set_index++) {
+        free(cache[set_index]);
+        cache[set_index] = NULL;
+    }
+    free(cache);
+    cache = NULL;
+    fclose(fp);
+
+    /* Output the hit and miss statistics for the autograder */
+    printf("hits:%d misses:%d evictions:%d\n", hit_count, miss_count, eviction_count);
+
+    exit( EXIT_SUCCESS );
+}
