32d73ee05fc80a0c86afd00bc0132d6bc09dec2b
Yipeng Huang <yipeng.huang@rutgers.edu>
2021-04-11 20:49:58 -0400

    Grading infrastructure.

--- pa5/assignment_autograder.py
+++ pa5/assignment_autograder.py
@@ -3,7 +3,7 @@
 from fullyAssociative import autograder as fullyAssociative_autograder
 from directMapped import autograder as directMapped_autograder
 from setAssociative import autograder as setAssociative_autograder
-from loopInterchange import autograder as loopInterchange_autograder
+from cacheBlocking import autograder as cacheBlocking_autograder
 from cacheOblivious import autograder as cacheOblivious_autograder
 
 total = 0
@@ -11,7 +11,7 @@ total = 0
 total += fullyAssociative_autograder.grade_fullyAssociative ( path="fullyAssociative/", verbose=False )
 total += directMapped_autograder.grade_directMapped ( path="directMapped/", verbose=False )
 total += setAssociative_autograder.grade_setAssociative ( path="setAssociative/", verbose=False )
-total += loopInterchange_autograder.grade_loopInterchange ( path="loopInterchange/", verbose=False )
+total += cacheBlocking_autograder.grade_cacheBlocking ( path="cacheBlocking/", verbose=False )
 total += cacheOblivious_autograder.grade_cacheOblivious ( path="cacheOblivious/", verbose=False )
 
 print ("Score on assignment: {} out of 120.".format(total))
